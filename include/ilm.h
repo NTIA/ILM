fd13cfc806a8da68fccb0e46c18a6ae04e9ff115
ErikHillNTIA <ehill@ntia.doc.gov>
2024-10-28 16:09:39 -0600

    .dll export rework
    Also documented and formatted.

--- include/ilm.h
+++ include/ilm.h
@@ -1,75 +1,364 @@
+#pragma once
+/**
+@file
+
+@brief
+ITU-R SG3 Irregular Lunar Model (ILM).
+
+The ILM is a modification of the NTIA/ITS Irregular Terrain Model (ITM) to be
+applicable to the surface of the Earth's moon.
+
+@author
+*/
+
+// ILM.dll header guard.
+#ifndef ILM_H
+#define ILM_H
+
 /* Standard includes. */
 #include <complex>
+
+/**
+@brief
+ILM.dll version number.
+*/
+#define VERSION R"(0.0.0)"
+/**
+@brief
+ILM.dll compilation date and time.
+*/
+#define COMPILE_TIME __DATE__ " " __TIME__
 
 /* Usings. */
 using namespace std;
 
-// Export the DLL functions as "C" and not C++
-#define DLLEXPORT extern "C" __declspec(dllexport)
+/* Utility functions. */
+/**
+@brief
+Maximum of x and y.
+*/
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
+/**
+@brief
+Minimum of x and y.
+*/
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+/**
+@brief
+Dim of x and y.
+*/
 #define DIM(x, y) (((x) > (y)) ? (x - y) : (0))
 
-#define PI                                      3.1415926535897932384
-#define a_e__meter                              1737400 // radius of moon
-#define THIRD                                   1.0 / 3.0
+/* Constants. */
+/**
+@brief
+The ratio of a circle's circumference to its diameter.
+*/
+#define PI 3.1415926535897932384
+/**
+@brief
+The radius of Earth's moon in meters.
+*/
+#define a_e__meter 1737400
+/**
+@brief
+The ratio 1/3.
+*/
+#define THIRD 1.0 / 3.0
 
-
-/////////////////////////////
-// Data Structures
-
+/**
+@brief
+Structure to hold intermediate values for debugging output.
+*/
 struct IntermediateValues
 {
-    double theta_hzn[2];        // Terminal horizon angles
-    double d_hzn__meter[2];     // Terminal horizon distances, in meters
-    double h_e__meter[2];       // Terminal effective heights, in meters
-    double delta_h__meter;      // Terrain irregularity parameter, in meters
-    double A_ref__db;           // Reference attenuation, in dB
-    double A_fs__db;            // Free space basic transmission loss, in dB
-    double d__km;               // Path distance, in km
-    int mode;                   // Mode of propagation value
+    /**
+    Terminal horizon angles.
+    */
+    double theta_hzn[2];
+
+    /**
+    Terminal horizon distances, in meters.
+    */
+    double d_hzn__meter[2];
+
+    /**
+    Terminal effective heights, in meters.
+    */
+    double h_e__meter[2];
+
+    /**
+    Terrain irregularity parameter, in meters.
+    */
+    double delta_h__meter;
+
+    /**
+    Reference attenuation, in dB.
+    */
+    double A_ref__db;
+
+    /**
+    Free space basic transmission loss, in dB.
+    */
+    double A_fs__db;
+
+    /**
+    Path distance, in km.
+    */
+    double d__km;
+
+    /**
+    Mode of propagation value.
+    */
+    int mode;
 };
 
-/////////////////////////////
-// Main ILM Functions
+/* DLL export/import. */
+/**
+_WIN32 indicates compilation on a Windows OS.
+Define _WIN32 in:
+Visual Studio > Projects > ILM_DLL Properties > C/C++ > Preprocessor
+> Preprocessor Definitions.
+*/
+#ifdef _WIN32
 
-DLLEXPORT int PointToPoint(double h_tx__meter, double h_rx__meter, double pfl[], double f__mhz,
-    int pol, double epsilon, double sigma, double p, double* A__db, long* warnings);
-DLLEXPORT int PointToPoint_Ex(double h_tx__meter, double h_rx__meter, double pfl[], double f__mhz,
-    int pol, double epsilon, double sigma, double p, double* A__db,
-    long* warnings, IntermediateValues* interValues);
-DLLEXPORT int Area(double h_tx__meter, double h_rx__meter, int tx_site_criteria, int rx_site_criteria, double d__km,
-    double delta_h__meter, double f__mhz, int pol, double epsilon, double sigma,
-    double p, double* A__db, long* warnings);
-DLLEXPORT int Area_Ex(double h_tx__meter, double h_rx__meter, int tx_site_criteria, int rx_site_criteria, double d__km,
-    double delta_h__meter, double f__mhz, int pol, double epsilon, double sigma,
-    double p, double* A__db, long* warnings, IntermediateValues* interValues);
+/**
+ILM_DLL_EXPORT indicates if the library functions should be exported or
+imported.
+Only define ILM_DLL_EXPORT to export the library functions for creating
+ILM.dll. Do not define ILM_DLL_EXPORT in projects that import ILM.dll and the
+library functions will be imported.
 
-/////////////////////////////
-// ILM Helper Functions
+Define ILM_DLL_EXPORT in:
+Visual Studio > Project > MKE Properties > C/C++ > Preprocessor
+> Preprocessor Definitions.
+*/
+#ifdef ILM_DLL_EXPORT
+#define ILM_API extern "C" __declspec(dllexport)
+#else
+#define ILM_API extern "C" __declspec(dllimport)
+#endif // ILM_DLL_EXPORT
+#else
+// Compiling with a non-Windows OS.
+#define ILM_API
+#endif // _WIN32
 
-DLLEXPORT double ComputeDeltaH(double pfl[], double d_start__meter, double d_end__meter);
-DLLEXPORT double DiffractionLoss(double d__meter, double d_hzn__meter[2], double h_e__meter[2], complex<double> Z_g,
-    double delta_h__meter, double h__meter[2], double theta_los, double f__mhz);
-DLLEXPORT void FindHorizons(double pfl[], double h__meter[2], double theta_hzn[2], double d_hzn__meter[2]);
-DLLEXPORT double FreeSpaceLoss(double d__meter, double f__mhz);
-DLLEXPORT double FresnelIntegral(double v2);
-DLLEXPORT double HeightFunction(double x__meter, double K);
-DLLEXPORT void InitializeArea(int site_criteria[2], double delta_h__meter,
-    double h__meter[2], double h_e__meter[2], double d_hzn__meter[2], double theta_hzn[2]);
-DLLEXPORT void InitializePointToPoint(double f__mhz, int pol, double epsilon, double sigma, complex<double>* Z_g);
-DLLEXPORT double InverseComplementaryCumulativeDistributionFunction(double q);
-DLLEXPORT double KnifeEdgeDiffraction(double d__meter, double f__mhz, double theta_los, double d_hzn__meter[2]);
-DLLEXPORT void LinearLeastSquaresFit(double pfl[], double d_start, double d_end, double *fit_y1, double *fit_y2);
-DLLEXPORT double LineOfSightLoss(double d__meter, double h_e__meter[2], complex<double> Z_g, double delta_h__meter,
-    double M_d, double A_d0, double d_sML__meter, double f__mhz);
-DLLEXPORT int LongleyRice(double theta_hzn[2], double f__mhz, complex<double> Z_g, double d_hzn__meter[2],
-    double h_e__meter[2], double delta_h__meter, double h__meter[2], double d__meter, double* A_ref__db, long* warnings, int* propmode);
-DLLEXPORT void QuickPfl(double pfl[], double h__meter[2], double theta_hzn[2], double d_hzn__meter[2], 
-    double h_e__meter[2], double *delta_h__meter, double *d__meter);
-DLLEXPORT double SigmaHFunction(double delta_h__meter);
-DLLEXPORT double SmoothEarthDiffraction(double d__meter, double f__mhz, double theta_los, 
-    double d_hzn__meter[2], double h_e__meter[2], complex<double> Z_g);
-DLLEXPORT double TerrainRoughness(double d__meter, double delta_h__meter);
-DLLEXPORT int ValidateInputs(double h_tx__meter, double h_rx__meter, double p, double f__mhz, int pol, double epsilon, double sigma, long* warnings);
-DLLEXPORT double Variability(double p, double delta_h__meter, double f__mhz, double d__meter, double A_ref__db);
+/* Prototypes. */
+
+ILM_API char const *version();
+
+ILM_API char const *compile_time();
+
+/* Primary ILM library functions. */
+
+ILM_API int PointToPoint(
+    double h_tx__meter,
+    double h_rx__meter,
+    double pfl[],
+    double f__mhz,
+    int pol,
+    double epsilon,
+    double sigma,
+    double p,
+    double* A__db,
+    long* warnings
+);
+
+ILM_API int PointToPoint_Ex(
+    double h_tx__meter,
+    double h_rx__meter,
+    double pfl[],
+    double f__mhz,
+    int pol,
+    double epsilon,
+    double sigma,
+    double p,
+    double* A__db,
+    long* warnings,
+    IntermediateValues* interValues
+);
+
+ILM_API int Area(
+    double h_tx__meter,
+    double h_rx__meter,
+    int tx_site_criteria,
+    int rx_site_criteria,
+    double d__km,
+    double delta_h__meter,
+    double f__mhz,
+    int pol,
+    double epsilon,
+    double sigma,
+    double p,
+    double* A__db,
+    long* warnings
+);
+
+ILM_API int Area_Ex(
+    double h_tx__meter,
+    double h_rx__meter,
+    int tx_site_criteria,
+    int rx_site_criteria,
+    double d__km,
+    double delta_h__meter,
+    double f__mhz,
+    int pol,
+    double epsilon,
+    double sigma,
+    double p,
+    double* A__db,
+    long* warnings,
+    IntermediateValues* interValues
+);
+
+/* ILM Helper Functions. */
+
+ILM_API double ComputeDeltaH(
+    double pfl[],
+    double d_start__meter,
+    double d_end__meter
+);
+
+ILM_API double DiffractionLoss(
+    double d__meter,
+    double d_hzn__meter[2],
+    double h_e__meter[2],
+    complex<double> Z_g,
+    double delta_h__meter,
+    double h__meter[2],
+    double theta_los,
+    double f__mhz
+);
+
+ILM_API void FindHorizons(
+    double pfl[],
+    double h__meter[2],
+    double theta_hzn[2],
+    double d_hzn__meter[2]
+);
+
+ILM_API double FreeSpaceLoss(
+    double d__meter,
+    double f__mhz
+);
+
+ILM_API double FresnelIntegral(
+    double v2
+);
+
+ILM_API double HeightFunction(
+    double x__meter,
+    double K
+);
+
+ILM_API void InitializeArea(
+    int site_criteria[2],
+    double delta_h__meter,
+    double h__meter[2],
+    double h_e__meter[2],
+    double d_hzn__meter[2],
+    double theta_hzn[2]
+);
+
+ILM_API void InitializePointToPoint(
+    double f__mhz,
+    int pol,
+    double epsilon,
+    double sigma,
+    complex<double> *Z_g
+);
+
+ILM_API double InverseComplementaryCumulativeDistributionFunction(
+    double q
+);
+
+ILM_API double KnifeEdgeDiffraction(
+    double d__meter,
+    double f__mhz,
+    double theta_los,
+    double d_hzn__meter[2]
+);
+
+ILM_API void LinearLeastSquaresFit(
+    double pfl[],
+    double d_start,
+    double d_end,
+    double *fit_y1,
+    double *fit_y2
+);
+
+ILM_API double LineOfSightLoss(
+    double d__meter,
+    double h_e__meter[2],
+    complex<double> Z_g,
+    double delta_h__meter,
+    double M_d,
+    double A_d0,
+    double d_sML__meter,
+    double f__mhz
+);
+
+ILM_API int LongleyRice(
+    double theta_hzn[2],
+    double f__mhz,
+    complex<double> Z_g,
+    double d_hzn__meter[2],
+    double h_e__meter[2],
+    double delta_h__meter,
+    double h__meter[2],
+    double d__meter,
+    double* A_ref__db,
+    long* warnings,
+    int* propmode
+);
+
+ILM_API void QuickPfl(
+    double pfl[],
+    double h__meter[2],
+    double theta_hzn[2],
+    double d_hzn__meter[2],
+    double h_e__meter[2],
+    double *delta_h__meter,
+    double *d__meter
+);
+
+ILM_API double SigmaHFunction(
+    double delta_h__meter
+);
+
+ILM_API double SmoothEarthDiffraction(
+    double d__meter,
+    double f__mhz,
+    double theta_los,
+    double d_hzn__meter[2],
+    double h_e__meter[2],
+    complex<double> Z_g
+);
+
+ILM_API double TerrainRoughness(
+    double d__meter,
+    double delta_h__meter
+);
+
+ILM_API int ValidateInputs(
+    double h_tx__meter,
+    double h_rx__meter,
+    double p,
+    double f__mhz,
+    int pol,
+    double epsilon,
+    double sigma,
+    long *warnings
+);
+
+ILM_API double Variability(
+    double p,
+    double delta_h__meter,
+    double f__mhz,
+    double d__meter,
+    double A_ref__db
+);
+
+#endif  // ILM_H
---
+++ src/ilm.cpp
@@ -0,0 +1,47 @@
+/**
+@file
+
+@brief
+This file contains functions to return the version and compile time of ITM.dll.
+
+@author
+*/
+
+/* Local includes. */
+#include "../include/ilm.h"
+
+/* Implementations. */
+
+/**
+Return the version of ILM.dll.
+
+INPUT
+    None
+
+OUTPUT
+    Returns a pointer to the VERSION character string.
+
+SUBROUTINES
+    None
+
+*/
+char const *version() {
+    return VERSION;
+};
+
+/**
+Return the compile time of ILM.dll.
+
+INPUT
+    None
+
+OUTPUT
+    Returns a pointer to the COMPILE_TIME character string.
+
+SUBROUTINES
+    None
+
+*/
+char const *compile_time() {
+    return COMPILE_TIME;
+};
